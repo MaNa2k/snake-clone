80135caadb1d91fc99f8ad297ef3c7273d6897d7
Rehman Dar <devadmrda@viewledger.onmicrosoft.com>
2024-02-25 22:51:46 +0100

    chore: testing rectangle collision

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 
 pygame.init()
 
@@ -12,46 +14,60 @@ screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
 xPos = 30
 yPos = 30
-rectWidth=100
-rectHeight=100
+rectWidth=25
+rectHeight=25
 player = pygame.Rect((xPos, yPos, rectWidth, rectHeight))
 
+enemy1 = pygame.Rect(random.randint(0, 500), random.randint(0, 300), 25, 25)
+enemy2 = pygame.Rect(random.randint(0, 500), random.randint(0, 300), 25, 25)
+enemy3 = pygame.Rect(random.randint(0, 500), random.randint(0, 300), 25, 25)
+enemy4 = pygame.Rect(random.randint(0, 500), random.randint(0, 300), 25, 25)
+
 run = True
-currentDirection = 3
 xDir = 1
 yDir = 0
 
+foodType = ["x","y","z"]
+foodDuration = 4
+
+bgColor = (0,0,0)
+
 while run:
     clock.tick(FPS)
-
-    player.move_ip(xDir,yDir)
-
-    screen.fill((0,0,0))
+    screen.fill((bgColor))
     pygame.draw.rect(screen, (100, 100, 100), player)
+    pygame.draw.rect(screen, (100,100,100), enemy1)
+    pygame.draw.rect(screen, (100,100,100), enemy2)
+    pygame.draw.rect(screen, (100,100,100), enemy3)
+    pygame.draw.rect(screen, (100,100,100), enemy4)
 
+    if player.colliderect(enemy1) or player.colliderect(enemy2) or player.colliderect(enemy3) or player.colliderect(enemy4):
+        bgColor = (139,0,0)
+    else:
+        bgColor = (0,0,0)
+
+        
     key = pygame.key.get_pressed()
 
-    if key[pygame.K_LEFT] == True:
-        currentDirection = 2
+    if key[pygame.K_LEFT] == True and xDir!=1:
         xDir=-1
         yDir=0
-    elif key[pygame.K_RIGHT] == True:
-        currentDirection = 3
+    elif key[pygame.K_RIGHT] == True and xDir!=-1:
         xDir=1
         yDir=0
-    elif key[pygame.K_UP] == True:
-        currentDirection = 0
+    elif key[pygame.K_UP] == True and yDir!=1:
         xDir=-0
         yDir=-1
-    elif key[pygame.K_DOWN] == True:
-        currentDirection = 1
+    elif key[pygame.K_DOWN] == True and yDir!=-1:
         xDir=0
         yDir=1
 
+    player.move_ip(xDir,yDir)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     pygame.display.update()
+
 pygame.quit()
